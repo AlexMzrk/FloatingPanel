d46fe7fcbf3594a1ba5eb3f97fea9689ca670109
Cube <cube@unitedtech.ai>
2024-09-12 14:18:55 +0200

    change min platform

--- Package.swift
+++ Package.swift
@@ -5,9 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "FloatingPanel",
-    platforms: [
-        .iOS(.v15)
-    ],
+    platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
